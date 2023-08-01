674a472f41d11497f50b162a340030d5a923a8f7
shalaevk <shalaevk1987@gmail.com>
2023-08-01 16:27:02 +0200

    chore: Remove commented code from Task component
    - Remove the commented useState and setCheckedHandler code
    - The Task component no longer uses the useState hook to manage the checked state
    
    This commit does not introduce any functional changes and is considered a chore.

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,10 +1,6 @@
 import { DeleteButton } from "./DeleteButton";
 
 export function Task({ text, deleter, id, completed, setCheckedHandler }) {
-  //   const [checked, setChecked] = useState(false);
-  //   function setCheckedHandler() {
-  //     setChecked(!checked);
-  //   }
   return (
     <li className="list-item" id={id}>
       <label className="list-item-label">
